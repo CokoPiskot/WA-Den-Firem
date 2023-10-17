c3e33de5b0bb6a3e07557e09f8031ae3e1d6e27b
David-K <krestan.david@gmail.com>
2023-10-17 22:14:15 +0200

    idk 1.0.3

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -2,3 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+class Ucebny(models.Model):
+    id_ucebny = models.IntegerField(primary_key=True)
+    nazev_ucebny = models.CharField(max_length=10)
+    kapacita = models.IntegerField()
+
+
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+
 
 # Create your views here.
 def index(request):
     return render(request, "myapp/main.html", {})
+
+def redirect_admin(request):
+    return redirect("/")

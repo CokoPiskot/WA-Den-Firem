c1e3342a6733a2193811f832e69f21eede36a703
David-K <krestan.david@gmail.com>
2023-12-15 23:38:24 +0100

    přídání __str__ metody pro lepší přehled v admin panelu

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Ucebny
+from .models import Ucebny, Firmy, Super
 
 # Register your models here.
 
 admin.site.register(Ucebny)
+admin.site.register(Firmy)
+admin.site.register(Super)
+
--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -3,8 +3,26 @@ from django.db import models
 # Create your models here.
 
 class Ucebny(models.Model):
-    id_ucebny = models.IntegerField(primary_key=True)
+    id_ucebny = models.AutoField(primary_key=True)
     nazev_ucebny = models.CharField(max_length=10)
     kapacita = models.IntegerField()
 
+    def __str__(self):
+        return f"{self.nazev_ucebny}"
 
+
+class Firmy(models.Model):
+    id_firmy = models.AutoField(primary_key=True)
+    nazev_firmy = models.CharField(max_length=45)
+    ucebna = models.ForeignKey(Ucebny, on_delete=models.PROTECT)
+    o_firme = models.CharField(max_length=100)
+    nazev_prezentace = models.CharField(max_length=100)
+    def __str__(self):
+        return f"{self.nazev_firmy}"
+
+
+class Super(models.Model):
+    login = models.CharField(max_length=45)
+    heslo = models.CharField(max_length=45)
+    def __str__(self):
+        return f"{self.login}"
